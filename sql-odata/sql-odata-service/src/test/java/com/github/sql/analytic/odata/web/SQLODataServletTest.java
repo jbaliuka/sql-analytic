90f58002e78ba9c44f17783b6866b1f5b142d79c
Juozas Baliuka <jbaliuka@gmail.com>
2016-04-25 00:39:54 +0300

    unused mocks

--- sql-odata/sql-odata-service/src/test/java/com/github/sql/analytic/odata/web/SQLODataServletTest.java
+++ sql-odata/sql-odata-service/src/test/java/com/github/sql/analytic/odata/web/SQLODataServletTest.java
@@ -12,11 +12,7 @@ import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Properties;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
-import javax.servlet.ServletOutputStream;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.apache.http.client.HttpClient;
@@ -37,68 +33,16 @@ import org.mockito.MockitoAnnotations;
 
 public class SQLODataServletTest {
 
-	private static final String url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";
-
-	private SQLODataServlet servlet;
-	@Mock
-	private ServletConfig config;
-	@Mock
-	private HttpServletRequest request;
-	@Mock
-	private HttpServletResponse response;
-	@Mock
-	private ServletOutputStream servletOutputStream;
+	private static final String url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1";		
 	private Connection connection;
 	@Mock
-	private DataSource datasource;
+	private DataSource datasource;	
 	private ODataClient client;
 	
-	private HttpClientFactory httpFactory = new HttpClientFactory() {
-		
-		@Override
-		public HttpClient create(HttpMethod method, URI uri) {
-			
-			return httpClient;
-		}
-		
-		@Override
-		public void close(HttpClient httpClient) {			
-			
-		}
-	};	
-
-	private HttpClient httpClient; 
-
-
-	@Before
-	public void init() throws ServletException, IOException, SQLException{
-		MockitoAnnotations.initMocks(this);	
-		connection = new org.h2.Driver().connect(url, new Properties());
-		try(InputStream in = getClass().getClassLoader().getResourceAsStream("mywind/northwind.sql")){
-			BufferedReader reader = new BufferedReader( new InputStreamReader(in));
-			connection.createStatement().execute(reader.readLine());			
-		}
-		try(InputStream in = getClass().getClassLoader().getResourceAsStream("mywind/northwind-data.sql")){
-			BufferedReader reader = new BufferedReader( new InputStreamReader(in));
-			connection.createStatement().execute(reader.readLine());			
-		}
-		stub(datasource.getConnection()).toReturn(connection);
-		servlet = new SQLODataServlet();
-		servlet.init(config);
-		servlet.setDatasource(datasource);
-		stub(response.getOutputStream()).toReturn(servletOutputStream);
-
-		client =  ODataClientFactory.getClient();		
-		client.getConfiguration().setHttpClientFactory(httpFactory);
-		client.getConfiguration().setDefaultPubFormat(ContentType.JSON);
-		httpClient = new MockHttpClient(servlet);
-
-	}
-
 	@Test
 	public void test() throws ServletException, IOException {
 
-		String serviceRoot = "http://localhost/SQLODataService.svc/";
+		String serviceRoot = "http://mock.server/SQLODataService.svc/";
 		ODataServiceDocumentRequest req =
 		client.getRetrieveRequestFactory().getServiceDocumentRequest(serviceRoot);
 		ODataRetrieveResponse<ClientServiceDocument> res = req.execute();
@@ -107,7 +51,7 @@ public class SQLODataServletTest {
 		
 		Collection<String> names = body.getEntitySetNames();
 		Assert.assertFalse(names.isEmpty());
-		
+	
 		
 	}
 
@@ -115,6 +59,54 @@ public class SQLODataServletTest {
 	@After
 	public void tearDown() throws Exception {
 		connection.close();
+	}
+	@Before
+	public void init() throws ServletException, IOException, SQLException{
+		
+		MockitoAnnotations.initMocks(this);		
+		connection = new org.h2.Driver().connect(url, new Properties());
+		stub(datasource.getConnection()).toReturn(connection);		
+		loadDB();
+		setupMockClient();
+
+	}
+
+
+	private void setupMockClient() {
+		
+		final SQLODataServlet servlet = new SQLODataServlet();		
+		servlet.setDatasource(datasource);
+		client =  ODataClientFactory.getClient();
+		client.getConfiguration().setDefaultPubFormat(ContentType.JSON);
+		
+		
+		 HttpClientFactory httpFactory = new HttpClientFactory() {
+			
+			@Override
+			public HttpClient create(HttpMethod method, URI uri) {
+				
+				return new MockHttpClient(servlet);
+			}
+			
+			@Override
+			public void close(HttpClient httpClient) {			
+				
+			}
+		};	
+		client.getConfiguration().setHttpClientFactory(httpFactory);
+	}
+
+
+	private void loadDB() throws SQLException, IOException {
+		
+		try(InputStream in = getClass().getClassLoader().getResourceAsStream("mywind/northwind.sql")){
+			BufferedReader reader = new BufferedReader( new InputStreamReader(in));
+			connection.createStatement().execute(reader.readLine());			
+		}
+		try(InputStream in = getClass().getClassLoader().getResourceAsStream("mywind/northwind-data.sql")){
+			BufferedReader reader = new BufferedReader( new InputStreamReader(in));
+			connection.createStatement().execute(reader.readLine());			
+		}
 	}
 
 
